d1f0d2ecd3c750b6400c820a6a6a861658a82424
Mohamed Bayoudh <almehdi.bayoudh@esprit.tn>
2023-11-14 22:28:40 +0100

    fuction

--- src/main/java/tn/esprit/demo1_almehdi_bayoudh_4twin7/repository/IReservationRepo.java
+++ src/main/java/tn/esprit/demo1_almehdi_bayoudh_4twin7/repository/IReservationRepo.java
@@ -2,10 +2,14 @@ package tn.esprit.demo1_almehdi_bayoudh_4twin7.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import tn.esprit.demo1_almehdi_bayoudh_4twin7.entity.Bloc;
+import tn.esprit.demo1_almehdi_bayoudh_4twin7.entity.etudiant;
 import tn.esprit.demo1_almehdi_bayoudh_4twin7.entity.reservation;
 
 import java.util.Date;
 import java.util.List;
 
 public interface IReservationRepo extends CrudRepository<reservation,Long> {
+    List<reservation> findChambreByReservations(etudiant e);
+
+    List<reservation> findByEtudiant(etudiant e);
 }
